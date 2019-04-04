f325a6d14be898734aa5c82323b3bda3560662f4
Cara-Sciorilli <csciorilli@gmail.com>
2019-04-04 12:09:00 -0400

    deploy files

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,11 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :task_tracker3, TaskTracker3Web.Endpoint,
-  http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "example.com", port: 80],
+  server: true,
+  root: ".",
+  version: Application.spec(:phoenix_distillery, :vsn),
+  http: [port: {:system, "PORT"}],
+  url: [host: "localhost", port: {:system, "PORT"}],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
@@ -68,4 +71,23 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs which should be versioned
 # separately.
-import_config "prod.secret.exs"
+get_secret = fn name ->
+  base = Path.expand("~/.config/task_tracker3")
+  File.mkdir_p!(base)
+  path = Path.join(base, name)
+  unless File.exists?(path) do
+    secret = Base.encode16(:crypto.strong_rand_bytes(32))
+    File.write!(path, secret)
+  end
+  String.trim(File.read!(path))
+end
+
+config :task_tracker3, TaskTracker3Web.Endpoint,
+  secret_key_base: get_secret.("key_base");
+
+# Configure your database
+config :task_tracker3, TaskTracker3.Repo,
+  username: "task_tracker3_redo",
+  password: "task_tracker3_redo",
+  database: "task_tracker3_redo",
+  pool_size: 15
